695e52e90d1343324fb9ff6037c13ccea48e94ab
Swastick Das <142610716+algoplutus1708@users.noreply.github.com>
2024-03-11 11:15:48 +0530

    Update 7229 Assignment_3.asm

--- 7229 Assignment_3.asm
+++ 7229 Assignment_3.asm
@@ -1,5 +1,4 @@
-;	Name    : Khushi
-;	Roll No : 7229
+
 ;	Write a switch case driven X86/64 ALP to perform 64-bit hexadecimal arithmetic operations(+,-,*,/) using suitable macros. Define procedure for each operation.
 ;	Date : 12/04/2022
 
