244ab42cb4339910861efda35a188128aa48683e
Swastick Das <142610716+algoplutus1708@users.noreply.github.com>
2024-03-17 19:14:39 +0530

    Update 7229 Assignment_5.asm

--- 7229 Assignment_5.asm
+++ 7229 Assignment_5.asm
@@ -1,5 +1,4 @@
-;	Name    : Dasari Hema Sree
-;	Roll NO : 7221
+
 ;	Write X86/64 ALP to convert 4-digit Hex number into its equivalent BCD number and 5- digit BCD number into its equivalent HEX number. Make your program user friendly to accept the choice from user for: (a) HEX to BCD b) BCD to HEX (c) EXIT.
 ;	Display proper strings to prompt the user while accepting the input and displaying the result. (Wherever necessary, use 64-bit registers).
 
